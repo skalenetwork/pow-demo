d048d9bf8ab317d36fa9471dd819cff02efc7656
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-03-03 20:43:05 +0200

    SKALE-3952 Add wallet support

--- pow-test.js
+++ pow-test.js
@@ -1,17 +1,17 @@
 const Web3 = require('web3');
 const mineGasForTransaction = require('./skale-miner').mineGasForTransaction;
 
-async function deploy(ownerKey){
+async function deploy(web3, ownerKey){
     let abi = JSON.parse('[{ "inputs": [ { "internalType": "uint256", "name": "x", "type": "uint256" } ], "name": "setA", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "a", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" } ]')
     let bytecode = "0x608060405234801561001057600080fd5b50610129806100206000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c8063ee919d50146037578063f0fdf834146062575b600080fd5b606060048036036020811015604b57600080fd5b810190808035906020019092919050505060a5565b005b608b60048036036020811015607657600080fd5b810190808035906020019092919050505060d3565b604051808215151515815260200191505060405180910390f35b600160008083815260200190815260200160002060006101000a81548160ff02191690831515021790555050565b60006020528060005260406000206000915054906101000a900460ff168156fea26469706673582212209507c7b274ebb9fcddca5c090dc4c124f45548dfbcdcd574d4017a19fb4eb67b64736f6c63430006000033"
     const myContract = new web3.eth.Contract(abi);
 
     let address = web3.eth.accounts.privateKeyToAccount(ownerKey)['address'];
-    return send(myContract.deploy({data:bytecode}), ownerKey, address);
+    return send(web3, myContract.deploy({data:bytecode}), ownerKey, address);
 }
 
 
-async function send(tr, pk, address) {
+async function send(web3, tr, pk, address) {
     let gas = await tr.estimateGas({"from": address});
     let nonce = await web3.eth.getTransactionCount(address)
     let tx = {
@@ -24,7 +24,7 @@ async function send(tr, pk, address) {
     return web3.eth.sendSignedTransaction(signed.rawTransaction)
 }
 
-async function test(testKey, ad){
+async function test(web3, testKey, ad){
     let address = web3.eth.accounts.privateKeyToAccount(testKey)['address'];
     let nonce = await web3.eth.getTransactionCount(address)
     let tx =  {
@@ -38,11 +38,26 @@ async function test(testKey, ad){
     return web3.eth.sendSignedTransaction(signed.rawTransaction)
 }
 
+async function testWallet(web3, ad){
+    let tx =  {
+        to: ad,
+        data: "0xee919d500000000000000000000000000000000000000000000000000000000000000017"
+    }
+    await mineGasForTransaction(web3, tx);
+    return await web3.eth.accounts.sendTransaction(tx)
+}
+
 async function run(endpoint, ownerKey, testKey) {
     console.log(endpoint, ownerKey, testKey);
-    web3 = new Web3(endpoint);
-    let addr = await deploy(ownerKey);
-    res = await test(testKey, addr.contractAddress);
+    if (!window.ethereum.selectedAddress) {
+        await window.ethereum.enable(); // <<< ask for permission
+    }
+    userAccount = window.ethereum.selectedAddress;
+    console.log(userAccount);
+    _web3 = new Web3(window.ethereum);
+    ownerWeb3 = new Web3(endpoint);
+    let addr = await deploy(ownerWeb3, ownerKey);
+    res = await test(_web3, testKey, addr.contractAddress);
     console.log('Transaction is sent:', res);
 }
 
